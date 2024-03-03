9089cb7bc8c5450ad72eeb06eadfb258ebc61a03
jnorambu <javinorambl@gmail.com>
2024-03-03 11:17:50 -0300

    new fetch url

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,20 +4,25 @@ import React from "react";
 export default function Home() {
 
   const [selectedFile, setSelectedFile] = React.useState(0);
+  const [selectedLanguage, setLanguage] = React.useState(0);
 
   function handleFileInputChange(event) {
     setSelectedFile(event.target.files[0]);
   }
 
+  function handleLanguageInputChange(event) {
+    setLanguage(event.target.value);
+  }
+
   function send(event){
     event.preventDefault();
+
 
     if (selectedFile) {
       const data = new FormData();
       data.append('image', selectedFile);
 
-      const url = 'http://127.0.0.1:5000';
-
+      const url = 'http://127.0.0.1:5000/'+selectedLanguage;
       let result = fetch(url, {
         method: 'POST',
         headers: {
@@ -41,6 +46,14 @@ export default function Home() {
 
       <div id="formulario">
         <form>
+        <div class="form-group">
+        <select name="languages" id="lang" onChange={handleLanguageInputChange}>
+          <option value="none">None</option>
+          <option value="japanese">Japanese</option>
+          <option value="korean">Korean</option>
+          <option value="russian">Russian</option>
+        </select>
+        </div>
         <div class="form-group">
           <label for="images" class="drop-container" id="dropcontainer">
             <span class="drop-title"></span>
